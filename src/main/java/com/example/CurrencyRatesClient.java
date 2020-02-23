61482586ed64684643ab028ad2929eeda4e5586e
Filip Kasprzycki <kasprzycki.filip@gmail.com>
2020-02-23 22:11:38 +0100

    refactor

--- src/main/java/com/example/CurrencyRatesClient.java
+++ src/main/java/com/example/CurrencyRatesClient.java
@@ -13,21 +13,12 @@ import java.time.format.DateTimeParseException;
 
 public class CurrencyRatesClient {
     public static void main(String[] args) throws IOException {
-        if (args == null || args.length != 1) {
-            throw new IllegalArgumentException("Podaj proszę początkową datę w formacie yyyy-MM-dd");
-        }
+        verifyArgumentsLength(args);
 
-        LocalDate fromDate;
-        try {
-            fromDate = LocalDate.parse(args[0]);
-        } catch(DateTimeParseException e) {
-            throw new IllegalArgumentException("Podaj proszę początkową datę w formacie yyyy-MM-dd");
-        }
+        LocalDate fromDate = getFromDate(args[0]);
         LocalDate toDate = LocalDate.now();
 
-        if(fromDate.compareTo(toDate) > 0) {
-            throw new IllegalArgumentException("Początkowa data powinna być wcześniejsza: " + fromDate);
-        }
+        verifyDate(fromDate, toDate);
 
         String urlString = String.format("http://api.nbp.pl/api/exchangerates/rates/C/USD/%s/%s", fromDate, toDate);
 
@@ -35,5 +26,27 @@ public class CurrencyRatesClient {
         InputStreamReader streamReader = new InputStreamReader(url.openStream());
         CurrencyRates currencyRates = new Gson().fromJson(streamReader, CurrencyRates.class);
         System.out.println(currencyRates);
+    }
+
+    private static void verifyDate(LocalDate fromDate, LocalDate toDate) {
+        if (fromDate.compareTo(toDate) > 0) {
+            throw new IllegalArgumentException("Początkowa data powinna być wcześniejsza: " + fromDate);
+        }
+    }
+
+    private static void verifyArgumentsLength(String[] args) {
+        if (args == null || args.length != 1) {
+            throw new IllegalArgumentException("Podaj proszę początkową datę w formacie yyyy-MM-dd");
+        }
+    }
+
+    private static LocalDate getFromDate(String arg) {
+        LocalDate fromDate;
+        try {
+            fromDate = LocalDate.parse(arg);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("Podaj proszę początkową datę w formacie yyyy-MM-dd");
+        }
+        return fromDate;
     }
 }
