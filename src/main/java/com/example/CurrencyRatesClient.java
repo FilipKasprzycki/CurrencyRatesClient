eb3b4df2faec3eccc0e5c22bade958104632a491
Filip Kasprzycki <kasprzycki.filip@gmail.com>
2020-02-23 21:53:59 +0100

    wyniesiono daty z uml do zmiennych

--- src/main/java/com/example/CurrencyRatesClient.java
+++ src/main/java/com/example/CurrencyRatesClient.java
@@ -7,10 +7,17 @@ import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.time.LocalDate;
+
 
 public class CurrencyRatesClient {
     public static void main(String[] args) throws IOException {
-        URL url = new URL("http://api.nbp.pl/api/exchangerates/rates/C/USD/2020-02-01/2020-02-23");
+        LocalDate fromDate = LocalDate.parse("2020-02-01");
+        LocalDate toDate = LocalDate.parse("2020-02-23");
+
+        String urlString = String.format("http://api.nbp.pl/api/exchangerates/rates/C/USD/%s/%s", fromDate, toDate);
+
+        URL url = new URL(urlString);
         InputStreamReader streamReader = new InputStreamReader(url.openStream());
         CurrencyRates currencyRates = new Gson().fromJson(streamReader, CurrencyRates.class);
         System.out.println(currencyRates);
