f9bc55e760308fed4a00c0f4fcbf4aa76a21b916
Filip Kasprzycki <kasprzycki.filip@gmail.com>
2020-02-23 22:07:21 +0100

    pobieranie daty z wejscia

--- src/main/java/com/example/CurrencyRatesClient.java
+++ src/main/java/com/example/CurrencyRatesClient.java
@@ -8,12 +8,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 
 public class CurrencyRatesClient {
     public static void main(String[] args) throws IOException {
-        LocalDate fromDate = LocalDate.parse("2020-02-01");
-        LocalDate toDate = LocalDate.parse("2020-02-23");
+        if (args == null || args.length != 1) {
+            throw new IllegalArgumentException("Podaj proszę początkową datę w formacie yyyy-MM-dd");
+        }
+
+        LocalDate fromDate;
+        try {
+            fromDate = LocalDate.parse(args[0]);
+        } catch(DateTimeParseException e) {
+            throw new IllegalArgumentException("Podaj proszę początkową datę w formacie yyyy-MM-dd");
+        }
+        LocalDate toDate = LocalDate.now();
+
+        if(fromDate.compareTo(toDate) > 0) {
+            throw new IllegalArgumentException("Początkowa data powinna być wcześniejsza: " + fromDate);
+        }
 
         String urlString = String.format("http://api.nbp.pl/api/exchangerates/rates/C/USD/%s/%s", fromDate, toDate);
 
